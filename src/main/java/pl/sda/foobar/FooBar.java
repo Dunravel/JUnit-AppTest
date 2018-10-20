1daadf0e9b50591444b422cde0f13a0bc913c8d9
Marcin Iskra <dunravel@gmail.com>
2018-10-20 12:07:55 +0200

    FooBar - Refactor: Changed names of variables and public to private for constants

--- src/main/java/pl/sda/foobar/FooBar.java
+++ src/main/java/pl/sda/foobar/FooBar.java
@@ -1,9 +1,12 @@
 package pl.sda.foobar;
 
 class FooBar {
+
+    private static final String REPRESENTATION_DIVIDABLE_BY_THREE = "Foo";
+
     String convertToString(int number) {
         if(number % 3 == 0){
-            return "Foo";
+            return REPRESENTATION_DIVIDABLE_BY_THREE;
         }
         return String.valueOf(number);
     }
--- src/test/java/pl/sda/foobar/FooBarTest.java
+++ src/test/java/pl/sda/foobar/FooBarTest.java
@@ -1,30 +1,38 @@
 package pl.sda.foobar;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FooBarTest {
 
-    public static final int NUMBER = 1;
-    public static final String EXPECTED_REPRESENTATION = "1";
+    private static final int NUMBER = 1;
+    private static final String EXPECTED_REPRESENTATION = "1";
+    private static final int DIVIDABLE_BY_THREE = 3;
+    private FooBar fooBar;
+
+    @Before
+    public void init(){
+
+        fooBar = new FooBar();
+    }
 
     @Test
     public void shouldReturnStringIntRepresentationWhenPutInt() {
         //given
 
         //when
-        String result = new FooBar().convertToString(NUMBER);
+        String result = fooBar.convertToString(NUMBER);
 
         //then
         Assert.assertEquals(EXPECTED_REPRESENTATION, result); // weryfikujemy czy jest równe
     }
 
     @Test
-    public void shouldRetirnStringFooWhenIntIsDividedByThree() {
+    public void shouldReturnStringFooWhenIntIsDividedByThree() {
         //given
-        int number = 3;
         //when
-        String result = new FooBar().convertToString(number);
+        String result = fooBar.convertToString(DIVIDABLE_BY_THREE);
         //then
         Assert.assertEquals("Foo", result);
     }
