077e878845d7de86fc711c94836ab442a59880a7
Marcin Iskra <dunravel@gmail.com>
2018-10-20 12:57:51 +0200

    FooBar - Refactor: extracted variables to constants

--- src/main/java/pl/sda/foobar/FooBar.java
+++ src/main/java/pl/sda/foobar/FooBar.java
@@ -4,18 +4,32 @@ class FooBar {
 
     private static final String REPRESENTATION_DIVIDABLE_BY_THREE = "Foo";
     private static final String REPRESENTATION_DIVIDABLE_BY_FIVE = "Bar";
+    private static final String REPRESENTATION_DIVIDABLE_BY_FIFTEEN = REPRESENTATION_DIVIDABLE_BY_THREE + REPRESENTATION_DIVIDABLE_BY_FIVE;
 
     String convertToString(int number) {
+
+
         if(number % 15 == 0){
-            return REPRESENTATION_DIVIDABLE_BY_THREE + REPRESENTATION_DIVIDABLE_BY_FIVE;
+            return REPRESENTATION_DIVIDABLE_BY_FIFTEEN;
+        }
+        if(number % 3 == 0){
+            return REPRESENTATION_DIVIDABLE_BY_THREE;
         }
         if(number % 5 == 0){
             return REPRESENTATION_DIVIDABLE_BY_FIVE;
         }
 
-        if(number % 3 == 0){
-            return REPRESENTATION_DIVIDABLE_BY_THREE;
-        }
         return String.valueOf(number);
+
+
+//        String result = "";
+//        if(number % 3 == 0){
+//            result += REPRESENTATION_DIVIDABLE_BY_THREE;
+//        }
+//        if(number % 5 == 0){
+//            result += REPRESENTATION_DIVIDABLE_BY_FIVE;
+//        }
+//
+//        return result.isEmpty() ? String.valueOf(number) : result;
     }
 }
