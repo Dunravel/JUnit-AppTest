a145e082dbeed661f1fffcedc6afce1764debc91
Marcin Iskra <dunravel@gmail.com>
2018-10-20 10:41:37 +0200

    Added basic test instructions

--- src/test/java/pl/sda/foobar/FooBarTest.java
+++ src/test/java/pl/sda/foobar/FooBarTest.java
@@ -1,6 +1,20 @@
 package pl.sda.foobar;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FooBarTest {
+    @Test
+    public void shouldReturnStringIntRepresentationWhenPutInt(){
+        //given
+            int x = 1;
+        //when
+            String z = new FooBar().y(x);
+
+        //then
+        Assert.assertEquals("1",z); // weryfikujemy czy jest równe
+
+    }
 
 
 }
